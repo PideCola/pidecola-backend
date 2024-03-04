02c8132eaba38d3a6860267137902e59d49025cb
Miguel Perez <miguepersa@gmail.com>
2024-03-04 17:56:34 -0400

    rides app admin site setup

--- rides/admin.py
+++ rides/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from rides.models import Route, RideRequest, Ride
 
 # Register your models here.
+admin.site.register(Route, admin.ModelAdmin)
+admin.site.register(RideRequest, admin.ModelAdmin)
+admin.site.register(Ride, admin.ModelAdmin)
