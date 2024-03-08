11bf3030f06cde5c139a374f6b4088b3dbfa3fe7
ChuyB <bovea3359@gmail.com>
2024-03-08 13:27:52 -0400

    add origin and destination to ride_request

--- rides/models.py
+++ rides/models.py
@@ -17,12 +17,13 @@ class Ride(models.Model):
     )
 
     driver = models.ForeignKey(User, on_delete=models.DO_NOTHING, related_name='rides')
-    route = models.ForeignKey(Route, on_delete=models.DO_NOTHING)
     created_at = models.DateTimeField(auto_now=True)
-    status = models.CharField(max_length=16, choices=STATUS_OPTIONS)
+    status = models.CharField(max_length=16, choices=STATUS_OPTIONS, default=PENDING)
 
 class RideRequest(models.Model):
     user = models.ForeignKey(User, related_name='ride_requests', on_delete=models.DO_NOTHING)
-    route = models.ForeignKey(Route, on_delete=models.DO_NOTHING)
+    origin = models.ForeignKey(Route, related_name='ride_origin', on_delete=models.DO_NOTHING)
+    destination = models.ForeignKey(Route, related_name='ride_destination', on_delete=models.DO_NOTHING)
     timestamp = models.DateTimeField(auto_now=True)
     ride = models.ForeignKey(Ride, on_delete=models.SET_NULL, null=True)
+
