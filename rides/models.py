2a365e8f39ae5f70fe10a487bd54b813f22e243e
ChuyB <bovea3359@gmail.com>
2024-03-11 09:50:28 -0400

    refactor: changes status options to spanish. Add cancelled option

--- rides/models.py
+++ rides/models.py
@@ -7,13 +7,15 @@ class Route(models.Model):
 
 class Ride(models.Model):
 
-    PENDING = 'pending'
-    STARTED = 'started'
-    FINISHED = 'finished'
+    PENDING = 'pendiente'
+    STARTED = 'iniciado'
+    FINISHED = 'finalizado'
+    CANCELLED = 'cancelado'
     STATUS_OPTIONS = (
         (PENDING, 'Pendiente'),
         (STARTED, 'Iniciado'),
-        (FINISHED, 'Finalizado')
+        (FINISHED, 'Finalizado'),
+        (CANCELLED, 'Cancelado')
     )
 
     driver = models.ForeignKey(User, on_delete=models.DO_NOTHING, related_name='rides')
@@ -21,9 +23,20 @@ class Ride(models.Model):
     status = models.CharField(max_length=16, choices=STATUS_OPTIONS, default=PENDING)
 
 class RideRequest(models.Model):
+    PENDING = 'pendiente'
+    STARTED = 'iniciado'
+    FINISHED = 'finalizado'
+    CANCELLED = 'cancelado'
+    STATUS_OPTIONS = (
+        (PENDING, 'Pendiente'),
+        (STARTED, 'Iniciado'),
+        (FINISHED, 'Finalizado'),
+        (CANCELLED, 'Cancelado')
+    )
     user = models.ForeignKey(User, related_name='ride_requests', on_delete=models.DO_NOTHING)
     origin = models.ForeignKey(Route, related_name='ride_origin', on_delete=models.DO_NOTHING)
     destination = models.ForeignKey(Route, related_name='ride_destination', on_delete=models.DO_NOTHING)
     timestamp = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=16, choices=STATUS_OPTIONS, default=PENDING)
     ride = models.ForeignKey(Ride, on_delete=models.SET_NULL, null=True)
 
