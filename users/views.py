5e00c3b25ecfd1c72bc31a3dc3fb156454528aea
luiss-23 <lblancog23@gmail.com>
2024-03-12 22:39:40 -0400

    endopoint to get vehicles

--- users/views.py
+++ users/views.py
@@ -12,6 +12,12 @@ class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
     queryset = User.objects.all()
 
+    @action(detail=True, methods=['get'])
+    def vehicles(self, request, pk):
+        user = User.objects.get(pk=pk)
+        vehicles = VehicleSerializer(user.vehicles.all(), many=True).data
+        return Response(vehicles, status=status.HTTP_200_OK)
+
 
 class VehicleViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated]
